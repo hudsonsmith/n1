4dd1c97c3d9f97ac6131cd453faf1a3cae4fcf90
Hudson Smith <hudson.smith.311@gmail.com>
2023-05-04 15:27:02 +0000

    Fixing sourcing problem.

--- main.sh
+++ main.sh
@@ -3,13 +3,23 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "${SCRIPT_DIR}"
 
+function source_all() {
+    # ${1}: The directory to source all the files from.
+
+    for file in $(ls "${1}"); do
+        source "${1}/${file}"
+    done
+}
+
 # Dynamically load the libs even if in dev mode.
 if [[ "${SCRIPT_DIR}" == "${HOME}/.omb" ]]; then
+    echo "Real Mode"
     # Source the entire library.
-    source ~/.omb/src/*
+    source_all ~/.omb/src
     THEME_DIR="$HOME/.omb/themes"
 else
-    source ./src/*
+    echo "Dev mode"
+    source_all ./src
     THEME_DIR="./themes"
 fi
 
@@ -17,7 +27,7 @@ function main() {
     if (( $# <= 0 )); then
         echo "Error: No arguments provided"
     else
-        case "$1" in
+        case "${1}" in
             set)
                 # Pass the theme dir and the name of the script to the api.
                 src::load_theme "${THEME_DIR}" "${2}"
--- themes/arrow.sh
+++ themes/arrow.sh
@@ -5,4 +5,4 @@ source ~/.omb/prompt_tools/*
 user="${USER:0:1}"
 hostname="${HOSTNAME:0:1}"
 
-export PS1="${reset}${white}${bold}[${cyan}${user}${red}@${cyan}${hostname}${white}] ${git_stats}${bold}${red}➜${cyan}${italic}  "
+export PS1="${reset}${white}${bold}[${cyan}${user}${red}@${cyan}${hostname}${white}] ${git_stats}${bold}${red}➜${reset}  "
