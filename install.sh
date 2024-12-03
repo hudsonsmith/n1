8b8e7a7a39362b6e7beb3b9481195acb54147b09
Hudson Smith <hudsonsmith@users.noreply.github.com>
2024-12-03 11:42:10 -0600

    Trying another

--- install.sh
+++ install.sh
@@ -19,17 +19,17 @@ function copy_files() {
 
     util::alert "Creating directories..."
 
-    mkdir -p "${INSTALLED_PATH}" || exit 1
+    mkdir -p "${INSTALLED_PATH}"
     cp main.sh "${INSTALLED_PATH}/n1"
 
-    mkdir -p "${INSTALLED_PATH}/themes" || exit 1
-    cp themes/* "${INSTALLED_PATH}/themes/" || exit 1
+    mkdir -p "${INSTALLED_PATH}/themes"
+    cp themes/* "${INSTALLED_PATH}/themes/"
 
-    mkdir -p "${INSTALLED_PATH}/src" || exit 1
-    cp src/* "${INSTALLED_PATH}/src" || exit 1
+    mkdir -p "${INSTALLED_PATH}/src"
+    cp src/* "${INSTALLED_PATH}/src"
 
-    mkdir -p "${INSTALLED_PATH}/prompt_tools" || exit 1
-    cp prompt_tools/* "${INSTALLED_PATH}/prompt_tools/" || exit 1
+    mkdir -p "${INSTALLED_PATH}/prompt_tools"
+    cp prompt_tools/* "${INSTALLED_PATH}/prompt_tools/"
 
     alias_text="alias n1='source ${INSTALLED_PATH}/n1'"
     if ! grep -Fxq "${alias_text}" ~/.bashrc; then
