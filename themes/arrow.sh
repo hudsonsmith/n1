4f754c65cdc2132727bd17d29c029cb02514d4a2
hudson <>
2024-12-03 20:22:24 -0600

    Improving arrow prompt to have git stats

--- themes/arrow.sh
+++ themes/arrow.sh
@@ -1,7 +1,36 @@
 #! /usr/bin/env bash
 
+source ~/.n1/prompt_tools/git.sh
+
+git_stats () {
+	if [[ ! $(__git_ps1) ]]; then
+		return
+	fi
+
+	local STATUS=$(git status -s 2> /dev/null)
+
+	if echo "$STATUS" | grep -q "fatal"; then
+		return
+	fi
+
+	local UNTRACKED=$(echo "$STATUS" | grep "^??" | wc -l)
+	local MODIFIED=$(echo "$STATUS" | grep "^ M\|^MM" | wc -l)
+	local DELETED=$(echo "$STATUS" | grep "^ D" | wc -l)
+
+	stats=""
+
+	#if [ $MODIFIED != 0 ]; then
+	stats="$green$MODIFIED~$reset "
+	#fi
+	#if [ $UNTRACKED != 0 ]; then
+	stats+="$red$DELETED-$reset "
+	stats+="$purple$UNTRACKED!$reset "
+	#fi
+	echo -e "$stats"
+}
+
 user="${USER:0:1}"
 hostname="${HOSTNAME:0:1}"
 
-export PS1="${reset}${white}${bold}[${cyan}${user}${red}@${cyan}${hostname}${white}] ${git_stats}${bold}${red}➜${reset}  "
+export PS1="${reset}${white}${bold}[${cyan}${user}${red}@${cyan}${hostname}${white} ${green}\W${white}] \$(git_stats)${bold}${white}${bold}${red}➜${reset}  "
 preview="${reset}${white}${bold}[${cyan}${user}${red}@${cyan}${hostname}${white}] ${git_stats}${bold}${red}➜${reset}  "
